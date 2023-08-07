b19603b8d177e417d5a1f3e264137515fb88f1f2
Yash Baddi <yashbaddi29@gmail.com>
2023-08-07 09:59:26 +0530

    Updated app.js with routes and cors

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,19 @@
 import express from "express";
-import config from "./config";
+import cors from "cors";
+import config from "./config.js";
+import restaurantsRouter from "./routes/restaurants.js";
+import ordersRouter from "./routes/orders.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use("/restaurants", restaurantsRouter);
+app.use("/orders", ordersRouter);
 
 app.listen(config.app.port, () => {
   console.log(`applicaton Listening to port 3000 ${config.app.port}`);
