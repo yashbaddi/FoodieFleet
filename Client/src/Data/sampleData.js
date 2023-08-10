9479fceb80aca3e58cce994a5d865675fcd2e174
Yash Baddi <yashbaddi29@gmail.com>
2023-08-11 00:25:39 +0530

    ✨Added Sampled Data to client

---
+++ Client/src/Data/sampleData.js
@@ -0,0 +1,49 @@
+export let orderID = "23d9d246-71e9-469c-9204-25eb8c91db92";
+
+export const sampleRestaurant = [
+  {
+    id: 1,
+    name: "Meghana Foods",
+    description: "The Delightful Hydrabadi Biryani's",
+  },
+  {
+    id: 2,
+    name: "MTR",
+    description: "Traditional Bengaluru Breakfast",
+  },
+];
+
+export const sampleItems = {
+  1: [
+    {
+      id: 1,
+      name: "Boneless Chicken Biryani",
+      vegitarian: false,
+      description: "Chicken Biryani with no bones",
+      price: 300,
+    },
+    {
+      id: 2,
+      name: "Boned Chicken Biryani",
+      vegitarian: false,
+      description: "Chicken Biryani with Bones",
+      price: 250,
+    },
+  ],
+  2: [
+    {
+      id: 3,
+      name: "Rava Idly",
+      description: "The Original Rava Idly",
+      price: 50,
+    },
+    {
+      id: 4,
+      name: "Masala Dosa",
+      description: "Not Overpriced at all",
+      price: 120,
+    },
+  ],
+};
+
+export const sampleOrders = [];
