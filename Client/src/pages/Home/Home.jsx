8839e4e54b762efb9e101032da749c61568769e0
Yash Baddi <yashbaddi29@gmail.com>
2023-08-11 00:49:21 +0530

    ⚡️Updated Home Component

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import { sampleRestaurant } from "../../hooks/data";
+import { useEffect, useState } from "react";
 import RestaurantList from "./RestaurantList";
+import { getAllRestaurants } from "../../services/requests";
 
 export default function Home() {
+  const [restaurants, setRestaurants] = useState([]);
+  console.log("Restaurant Data:", restaurants);
+  useEffect(() => {
+    getAllRestaurants().then((data) => setRestaurants(data));
+  }, []);
   return (
     <>
       Home Page
-      <RestaurantList restaurants={sampleRestaurant} />
+      <RestaurantList restaurants={restaurants} />
     </>
   );
 }
