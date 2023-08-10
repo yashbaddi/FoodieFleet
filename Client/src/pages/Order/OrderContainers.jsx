e4a224d74c4629c71dd50f2bcfd03da85d78692c
Yash Baddi <yashbaddi29@gmail.com>
2023-08-11 01:13:02 +0530

    ✨Added Order Container Component

---
+++ Client/src/pages/Order/OrderContainers.jsx
@@ -0,0 +1,12 @@
+export default function OrderContainer(props) {
+  console.log("OrderContainerProps", props);
+  return (
+    <>
+      <div className="border mx-3 my-1 p-2">
+        <h1>{props.item.name}</h1>
+        <p>Price:{props.item.price}</p>
+        <p>quantity:{props.quantity}</p>
+      </div>
+    </>
+  );
+}
