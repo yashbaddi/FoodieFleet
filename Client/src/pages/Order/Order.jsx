d51efdc9c4321c7f513cf8f72c8e933ba78c62db
Yash Baddi <yashbaddi29@gmail.com>
2023-08-11 01:12:45 +0530

    ✨Added Order Page

--- Client/src/pages/Order/Order.jsx
+++ Client/src/pages/Order/Order.jsx
@@ -1,10 +1,27 @@
-import OrderList from "./OrderList";
+import { useEffect, useState } from "react";
+import { orderID } from "../../Data/sampleData";
+import OrderContainer from "./OrderContainers";
+import { createOrder, readOrder } from "../../services/requests";
 
 export default function Order() {
+  const [ordersData, setOrderData] = useState([]);
+  console.log("Ordered Data", ordersData);
+
+  useEffect(() => {
+    readOrder(orderID).then((data) => setOrderData(data));
+  }, []);
   return (
     <>
-      Orders
-      <OrderList />
+      <h1>Orders</h1>
+      {ordersData.map((orderedItem, index) => {
+        return (
+          <OrderContainer
+            key={index}
+            item={orderedItem.items}
+            quantity={orderedItem.quantity}
+          />
+        );
+      })}
     </>
   );
 }
