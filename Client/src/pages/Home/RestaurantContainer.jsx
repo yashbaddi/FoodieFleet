7de01e1a4c30de29543a03119a545949d70459c5
Yash Baddi <yashbaddi29@gmail.com>
2023-08-11 00:50:05 +0530

    ⚡️Updated Restaurant Container Component

--- Client/src/pages/Home/RestaurantContainer.jsx
+++ Client/src/pages/Home/RestaurantContainer.jsx
@@ -4,10 +4,10 @@ export default function RestaurantContainer(props) {
   console.log("Restaurant props", props);
   return (
     <>
-      <Link to={`/restaurant/${props.id}`}>
+      <Link to={`/restaurant/${props.restaurant.id}`}>
         <div className="border mx-3 my-1 p-2">
-          <h3>{props.name}</h3>
-          <p>{props.desc}</p>
+          <h3>{props.restaurant.name}</h3>
+          <p>{props.restaurant.description}</p>
         </div>
       </Link>
     </>
