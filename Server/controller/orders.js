2b405f781deb1901b2ae27a52d82d43e9d2d4070
Yash Baddi <yashbaddi29@gmail.com>
2023-08-11 00:08:59 +0530

    ✨Added createEmpty order

--- Server/controller/orders.js
+++ Server/controller/orders.js
@@ -3,3 +3,10 @@ import { updateOrder, readOrder, createOrder } from "../model/orders.js";
 export async function getOrdersByOrderID(req, res) {
   res.json(await readOrder({ id: req.params.id }));
 }
+
+export async function createEmptyOrder(req, res) {
+  const response = await createOrder("8968071c-4f3d-4fb9-87f8-4f2ccba4c318", {
+    restaurantID: req.body.restaurantID,
+  });
+  res.json(response);
+}
