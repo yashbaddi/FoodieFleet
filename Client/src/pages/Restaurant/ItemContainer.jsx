ec7c1d75d33f1a6b65aa1db50f393fddf317a5bd
Yash Baddi <yashbaddi29@gmail.com>
2023-08-11 09:52:43 +0530

    🔨Fixed Add item and remove item in cart client

--- Client/src/pages/Restaurant/ItemContainer.jsx
+++ Client/src/pages/Restaurant/ItemContainer.jsx
@@ -1,19 +1,32 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { orderID } from "../../Data/sampleData";
+import { addItemToOrder, removeItemFromOrder } from "../../services/requests";
 
 export default function ItemContainer(props) {
+  const [quantity, setQuantity] = useState(0);
   console.log("Item Conosle:", props);
+  async function addItem() {
+    const res = await addItemToOrder(orderID, props.item.id);
+    setQuantity(res.quantity);
+  }
+
+  async function removeItem() {
+    const res = await removeItemFromOrder(orderID, props.item.id);
+    setQuantity(res.quantity);
+  }
   return (
     <>
-      <Link
-        to={`/restaurant/${"props.restaurantID"}/cusine/${"props.cusineID"}`}
-      >
-        <div className="border mx-3 my-1 p-2">
-          <h3>{props.name}</h3>
-          <p>{props.desc}</p>
-          <p>vegitarian:{props.vegitarian}</p>
-          <p>price:{props.price}</p>
+      <div className="border mx-3 my-1 p-2">
+        <h3>{props.item.name}</h3>
+        <p>{props.item.description}</p>
+        <p>vegitarian:{props.item.is_vegitarian}</p>
+        <p>price:{props.item.price}</p>
+        <div className="border">
+          <button onClick={addItem}>+</button>
+          <text>{quantity}</text>
+          <button onClick={removeItem}>-</button>
         </div>
-      </Link>
+      </div>
     </>
   );
 }
