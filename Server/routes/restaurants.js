818fd0846db8e8bdb08ac6e8997c6dbe31ca0136
Yash Baddi <yashbaddi29@gmail.com>
2023-08-12 12:38:34 +0530

    ✨Added Restaurant Routes

--- Server/routes/restaurants.js
+++ Server/routes/restaurants.js
@@ -1,16 +1,38 @@
 import express from "express";
 import {
+  createARestaurant,
+  createItemForRestaurant,
+  deleteItemInRestaurant,
+  deleteRestaurantByID,
   getAllRestaurants,
   getMenuOfRestaurant,
   getRestaurantsByID,
+  updateItemOfRestaurant,
+  updateRestaurantByID,
 } from "../controller/restaurants.js";
+import bodyParser from "body-parser";
 
 const restaurantsRouter = express.Router();
 
-restaurantsRouter.route("/").get(getAllRestaurants);
+restaurantsRouter
+  .route("/")
+  .get(getAllRestaurants)
+  .post(bodyParser.json(), createARestaurant);
 
-restaurantsRouter.route("/:id").get(getRestaurantsByID);
+restaurantsRouter
+  .route("/:id")
+  .get(getRestaurantsByID)
+  .put(bodyParser.json(), updateRestaurantByID)
+  .delete(deleteRestaurantByID);
 
-restaurantsRouter.route("/:id/items").get(getMenuOfRestaurant);
+restaurantsRouter
+  .route("/:restaurantID/items")
+  .get(getMenuOfRestaurant)
+  .post(bodyParser.json(), createItemForRestaurant);
+
+restaurantsRouter
+  .route("/:restaurantID/items/:itemID")
+  .put(bodyParser.json(), updateItemOfRestaurant)
+  .delete(deleteItemInRestaurant);
 
 export default restaurantsRouter;
