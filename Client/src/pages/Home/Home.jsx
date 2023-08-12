d9d5863437a0b92b016cb9d17b320a89f01d274f
Yash Baddi <yashbaddi29@gmail.com>
2023-08-12 12:40:27 +0530

    ⚡️Modified Home component

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react";
 import RestaurantList from "./RestaurantList";
 import { getAllRestaurants } from "../../services/requests";
+import CreateRestaurantForm from "./createRestaurantForm";
 
 export default function Home() {
   const [restaurants, setRestaurants] = useState([]);
+  const [isRestaurantFormVisible, setRestaurantFormVisible] = useState(false);
   console.log("Restaurant Data:", restaurants);
   useEffect(() => {
     getAllRestaurants().then((data) => setRestaurants(data));
   }, []);
+
+  function toggleFormComponentVisiblity() {
+    setRestaurantFormVisible(!isRestaurantFormVisible);
+  }
+  console.log(isRestaurantFormVisible);
   return (
     <>
       Home Page
+      <button onClick={toggleFormComponentVisiblity}>
+        Create a Restaurant
+      </button>
+      {isRestaurantFormVisible && <CreateRestaurantForm />}
       <RestaurantList restaurants={restaurants} />
     </>
   );
