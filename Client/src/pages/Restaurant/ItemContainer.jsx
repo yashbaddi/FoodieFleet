21903332413d80ebf6ed14713a82e164060c75f1
Yash Baddi <yashbaddi29@gmail.com>
2023-08-12 12:42:03 +0530

    ⚡️Modified ItemContainer Component

--- Client/src/pages/Restaurant/ItemContainer.jsx
+++ Client/src/pages/Restaurant/ItemContainer.jsx
@@ -1,32 +1,39 @@
 import { useState } from "react";
 import { orderID } from "../../Data/sampleData";
-import { addItemToOrder, removeItemFromOrder } from "../../services/requests";
+import {
+  addItemToOrder,
+  deleteItem,
+  removeItemFromOrder,
+} from "../../services/requests";
 
 export default function ItemContainer(props) {
   const [quantity, setQuantity] = useState(0);
   console.log("Item Conosle:", props);
-  async function addItem() {
+
+  async function deleteItemFromRestaurant() {
+    deleteItem(props.item.restaurant_id, props.item.id);
+  }
+  async function addOrderItem() {
     const res = await addItemToOrder(orderID, props.item.id);
     setQuantity(res.quantity);
   }
 
-  async function removeItem() {
+  async function removeOrderedItem() {
     const res = await removeItemFromOrder(orderID, props.item.id);
     setQuantity(res.quantity);
   }
   return (
-    <>
-      <div className="border mx-3 my-1 p-2">
-        <h3>{props.item.name}</h3>
-        <p>{props.item.description}</p>
-        <p>vegitarian:{props.item.is_vegitarian}</p>
-        <p>price:{props.item.price}</p>
-        <div className="border">
-          <button onClick={addItem}>+</button>
-          <text>{quantity}</text>
-          <button onClick={removeItem}>-</button>
-        </div>
+    <div className="border mx-3 my-1 p-2">
+      <h3>{props.item.name}</h3>
+      <p>{props.item.description}</p>
+      <p>vegitarian:{props.item.is_vegitarian}</p>
+      <p>price:{props.item.price}</p>
+      <div className="border">
+        <button onClick={addOrderItem}>+</button>
+        <text>{quantity}</text>
+        <button onClick={removeOrderedItem}>-</button>
       </div>
-    </>
+      <button onClick={deleteItemFromRestaurant}>Delete</button>
+    </div>
   );
 }
