74592ef09b7da3ba64efb108114c4e201816050c
Yash Baddi <yashbaddi29@gmail.com>
2023-08-12 12:42:22 +0530

    ⚡️Modified Resaurant Page

--- Client/src/pages/Restaurant/Restaurant.jsx
+++ Client/src/pages/Restaurant/Restaurant.jsx
@@ -2,17 +2,25 @@ import { useParams } from "react-router-dom";
 import ItemsList from "./ItemList";
 import { useEffect, useState } from "react";
 import { getRestaurant } from "../../services/requests";
+import CreateItemForm from "./createItemForm";
 
 export default function Restaurant() {
+  const [isItemFormVisible, setItemFormVisible] = useState(false);
   const { id } = useParams();
   const [restaurant, setRestaurant] = useState([]);
   useEffect(() => {
     getRestaurant(id).then((data) => setRestaurant(data));
   }, []);
 
+  function toggleFormComponentVisiblity() {
+    setItemFormVisible(!isItemFormVisible);
+  }
+
   console.log("rest:", restaurant);
   return (
     <>
+      <button onClick={toggleFormComponentVisiblity}>Create a Item</button>
+      {isItemFormVisible && <CreateItemForm restaurantID={id} />}
       <h1>{restaurant.name}</h1>
       <ItemsList id={id} />
     </>
