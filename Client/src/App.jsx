b4cf3b4c00eb9d4639cf33d5d6182c4e28e368e4
Yash Baddi <yashbaddi29@gmail.com>
2023-08-17 23:03:46 +0530

    ✨Added Redux

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import Home from "./pages/Home/Home";
 import Restaurant from "./pages/Restaurant/Restaurant";
 import Order from "./pages/Order/Order";
@@ -9,11 +13,13 @@ function App() {
   return (
     <>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/restaurant/:id" element={<Restaurant />} />
-        <Route path="/orders" element={<Order />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/restaurant/:id" element={<Restaurant />} />
+          <Route path="/orders" element={<Order />} />
+        </Routes>
+      </Provider>
     </>
   );
 }
