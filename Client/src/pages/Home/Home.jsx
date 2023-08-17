69f06b73b3f167e26d4b736c8c9e319f28daa23b
Yash Baddi <yashbaddi29@gmail.com>
2023-08-17 23:05:59 +0530

    ⚡️Modified Home Page to redux actions

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import RestaurantList from "./RestaurantList";
-import { getAllRestaurants } from "../../services/requests";
+
+// import { getAllRestaurants } from "../../services/requests";
 import CreateRestaurantForm from "./createRestaurantForm";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllRestaurants } from "../../store/actions/restaurants.action";
 
 export default function Home() {
-  const [restaurants, setRestaurants] = useState([]);
+  // const [restaurants, setRestaurants] = useState([]);
+  const restaurants = useSelector((state) => state.restaurants);
+  const dispatch = useDispatch();
   const [isRestaurantFormVisible, setRestaurantFormVisible] = useState(false);
   console.log("Restaurant Data:", restaurants);
 
   useEffect(() => {
-    getAllRestaurants().then((data) => setRestaurants(data));
-  }, []);
+    // getAllRestaurants().then((data) => setRestaurants(data));
+    console.log("In use Effect");
+    dispatch(getAllRestaurants());
+  }, [dispatch]);
 
   function toggleFormComponentVisiblity() {
     setRestaurantFormVisible(!isRestaurantFormVisible);
