d559a8dfc8b7219e875a6fbd1dabe13623714f77
Yash Baddi <yashbaddi29@gmail.com>
2023-08-22 14:13:37 +0530

    ⚡️Modified Restaurant List

--- Client/src/pages/Home/RestaurantList.jsx
+++ Client/src/pages/Home/RestaurantList.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import RestaurantContainer from "./RestaurantContainer";
+import { useDispatch } from "react-redux";
+import { getCartItemsAction } from "../../store/actionCreators/cart.action";
+import { getAllRestaurants } from "../../services/requests";
 
-export default function RestaurantList(props) {
+export default function RestaurantList() {
+  const [restaurants, setRestaurants] = useState([]);
+  const dispatch = useDispatch();
+  console.log("Restaurant Data:", restaurants);
+  dispatch(getCartItemsAction());
+
+  useEffect(() => {
+    getAllRestaurants().then((data) => setRestaurants(data));
+    console.log("In use Effect");
+    // dispatch(getAllRestaurants());
+  }, []);
+
+  function updateList(id) {
+    setRestaurants((restaurants) =>
+      restaurants.filter((restaurant) => restaurant.id !== id)
+    );
+  }
   return (
     <>
       List of Restaurant
-      {props.restaurants.map((restaurant, index) => {
-        return <RestaurantContainer key={index} restaurant={restaurant} />;
+      {restaurants.map((restaurant, index) => {
+        return (
+          <RestaurantContainer
+            key={index}
+            restaurant={restaurant}
+            onUpdate={updateList}
+          />
+        );
       })}
     </>
   );
