4975a2d1c18ebe4f2218bd76a28529c38f7be85d
Yash Baddi <yashbaddi29@gmail.com>
2023-08-22 14:12:28 +0530

    🧹Cleaned Home component

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,36 +1,14 @@
-import { useEffect, useState } from "react";
 import RestaurantList from "./RestaurantList";
-
-// import { getAllRestaurants } from "../../services/requests";
-import CreateRestaurantForm from "./createRestaurantForm";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllRestaurants } from "../../store/actions/restaurants.action";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  // const [restaurants, setRestaurants] = useState([]);
-  const restaurants = useSelector((state) => state.restaurants);
-  const dispatch = useDispatch();
-  const [isRestaurantFormVisible, setRestaurantFormVisible] = useState(false);
-  console.log("Restaurant Data:", restaurants);
-
-  useEffect(() => {
-    // getAllRestaurants().then((data) => setRestaurants(data));
-    console.log("In use Effect");
-    dispatch(getAllRestaurants());
-  }, [dispatch]);
-
-  function toggleFormComponentVisiblity() {
-    setRestaurantFormVisible(!isRestaurantFormVisible);
-  }
-  console.log(isRestaurantFormVisible);
   return (
     <>
       Home Page
-      <button onClick={toggleFormComponentVisiblity}>
-        Create a Restaurant
-      </button>
-      {isRestaurantFormVisible && <CreateRestaurantForm />}
-      <RestaurantList restaurants={restaurants} />
+      <Link to="/restaurant/new">
+        <button>Create a Restaurant</button>
+      </Link>
+      <RestaurantList />
     </>
   );
 }
