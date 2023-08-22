3bce5361ce48f7aaf99f721e8315e71c5334cbcd
Yash Baddi <yashbaddi29@gmail.com>
2023-08-22 14:12:06 +0530

    🔨Fixed Cart Reducer

--- Client/src/store/reducers/index.js
+++ Client/src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import restaurantsReducer from "./restaurants.reducer";
+import { cartReducer } from "./cart.reducer";
 
 export const rootReducer = combineReducers({
   restaurants: restaurantsReducer,
+  cart: cartReducer,
 });
