4edbfec2dc6ebe37b1fe19bad17662bb6a887731
Yash Baddi <yashbaddi29@gmail.com>
2023-08-24 14:03:33 +0530

    ⚡️Modified orders Controller for Service

--- Server/controller/orders.js
+++ Server/controller/orders.js
@@ -6,47 +6,40 @@ import {
   deleteItemFromOrder,
 } from "../model/orders.js";
 
-export default {
+import orderService from "../services/orders.js";
+
+const orderController = {
   getOrdersByOrderID: getOrdersByOrderID,
   createNewOrder: createNewOrder,
   patchCurrentOrder: patchCurrentOrder,
 };
 
 async function getOrdersByOrderID(req, res) {
-  res.json(await readOrder({ id: req.params.id }));
+  const response = orderService.getOrdersByOrderID(req.params.id);
+  res.json(response);
 }
 
 async function createNewOrder(req, res) {
-  const response = await createOrder("8968071c-4f3d-4fb9-87f8-4f2ccba4c318", {
-    restaurantID: req.body.restaurantID,
-  });
+  const response = orderService.createNewOrder(
+    "8968071c-4f3d-4fb9-87f8-4f2ccba4c318",
+    req.body.restaurantID
+  );
   res.json(response);
 }
 
 async function patchCurrentOrder(req, res) {
   if (req.body.item) {
-    if (req.body.item.quantity > 0) {
-      const updateResponse = await updateQuantity(
-        req.params.id,
-        req.body.item.id,
-        req.body.item.quantity
-      );
-
-      res.json(updateResponse);
-    }
-
-    if (req.body.item.quantity <= 0) {
-      const deleteResponse = await deleteItemFromOrder(
-        req.params.id,
-        req.body.item.id
-      );
-
-      res.json(deleteResponse);
-    }
+    const response = orderService.updateItemsInOrder(
+      req.params.id,
+      req.body.item
+    );
+    res.json(response);
   }
 
   if (!req.body.item) {
-    const response = await patchOrder(req.params.id, req.body);
+    const response = orderService.patchCurrentOrder(req.params.id, req.body);
     res.json(response);
   }
 }
+
+export default orderController;
