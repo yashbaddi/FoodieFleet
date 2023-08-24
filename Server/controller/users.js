c94b4d493b543f01cb111d755599d5e5e0de7783
Yash Baddi <yashbaddi29@gmail.com>
2023-08-24 16:09:47 +0530

    ✨Added Read User Commit

--- Server/controller/users.js
+++ Server/controller/users.js
@@ -2,9 +2,15 @@ import userService from "../services/users";
 
 const userController = {
   createUser: createUser,
+  readUser: readUser,
 };
 
 async function createUser(req, res) {
   const response = userService.createUser(res.body);
+  res.json(response);
+}
+
+async function readUser(req, res) {
+  const response = userService.readUserByID(req.params.id);
   res.json(response);
 }
