f983d595365297d73625bb50704b8c99f6fddf9c
Yash Baddi <yashbaddi29@gmail.com>
2023-08-24 13:45:33 +0530

    ⚡️ Modified Cart Service to work with Cart Control

--- Server/controller/cart.js
+++ Server/controller/cart.js
@@ -1,38 +1,24 @@
-import {
-  readItemsInCart,
-  removeItemFromCart,
-  updateQuantityInCart,
-} from "../model/cart.js";
+import cartService from "../services/cart.js";
 
-export default {
+const cartCorntroller = {
   updateCart: updateCart,
   readCart: readCart,
 };
 
 async function updateCart(req, res) {
-  if (req.body.item.quantity > 0) {
-    const updateResponse = await updateQuantityInCart(
-      "8968071c-4f3d-4fb9-87f8-4f2ccba4c318",
-      req.body.item.id,
-      req.body.item.quantity
-    );
-
-    res.json(updateResponse);
-  }
-
-  if (req.body.item.quantity <= 0) {
-    const deleteResponse = await removeItemFromCart(
-      "8968071c-4f3d-4fb9-87f8-4f2ccba4c318",
-      req.body.item.id
-    );
-
-    res.json(deleteResponse);
-  }
+  const updateResponse = await cartService.updateCart(
+    "8968071c-4f3d-4fb9-87f8-4f2ccba4c318",
+    req.body.item.id,
+    req.body.item.quantity
+  );
+  res.json(updateResponse);
 }
 
 async function readCart(req, res) {
-  const readResponse = await readItemsInCart(
+  const readResponse = await cartService.readCart(
     "8968071c-4f3d-4fb9-87f8-4f2ccba4c318"
   );
   res.json(readResponse);
 }
+
+export default cartCorntroller;
