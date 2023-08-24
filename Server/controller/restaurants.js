44195f016b2fc191738c51260d5a29e5f0770d8e
Yash Baddi <yashbaddi29@gmail.com>
2023-08-24 13:54:42 +0530

    ✨Updated Restaurant Controller with Service struct

--- Server/controller/restaurants.js
+++ Server/controller/restaurants.js
@@ -1,22 +1,10 @@
-import { get } from "lodash";
-import {
-  createItem,
-  deleteItem,
-  readItem,
-  updateItem,
-} from "../model/items.js";
-import {
-  createRestaurant,
-  deleteRestaurant,
-  readRestaurant,
-  updateRestaurant,
-} from "../model/restaurants.js";
+import restaurantService from "../services/restaurants.js";
 
-export default {
+const restaurantController = {
   getAllRestaurants: getAllRestaurants,
   getRestaurantsByID: getRestaurantsByID,
   getMenuOfRestaurant: getMenuOfRestaurant,
-  createARestaurant: createARestaurant,
+  createRestaurant: createRestaurant,
   createItemForRestaurant: createItemForRestaurant,
   updateItemOfRestaurant: updateItemOfRestaurant,
   deleteItemInRestaurant: deleteItemInRestaurant,
@@ -25,47 +13,64 @@ export default {
 };
 
 async function getAllRestaurants(req, res) {
-  const data = await readRestaurant();
-  const response = data.map((restaurant) => restaurant.data);
+  const response = await restaurantService.getAllRestaurants();
   res.json(response);
 }
 
 async function getRestaurantsByID(req, res) {
-  res.json(await readRestaurant({ id: req.params.id }));
+  const response = await restaurantService.getRestaurantsByID(req.params.id);
+  res.json(response);
 }
 
 async function getMenuOfRestaurant(req, res) {
-  res.json(await readItem(req.params.restaurantID));
+  const response = await restaurantService.getMenuOfRestaurant(
+    req.params.restaurantID
+  );
+  res.json(response);
 }
 
-async function createARestaurant(req, res) {
-  console.log("req body", req.body);
-  const response = await createRestaurant(
+async function createRestaurant(req, res) {
+  const response = await restaurantService.createRestaurant(
     "8968071c-4f3d-4fb9-87f8-4f2ccba4c318",
     req.body
   );
-  console.log(response);
-
   res.json(response);
 }
 
 async function createItemForRestaurant(req, res) {
-  const response = await createItem(req.params.restaurantID, req.body);
+  const response = await restaurantService.createItemForRestaurant(
+    req.params.restaurantID,
+    req.body
+  );
   res.json(response);
 }
 
 async function updateItemOfRestaurant(req, res) {
-  res.json(await updateItem(req.params.itemID, req.body));
+  const response = await restaurantService.updateItemOfRestaurant(
+    req.params.itemID,
+    req.body
+  );
+  res.json(response);
 }
 
 async function deleteItemInRestaurant(req, res) {
-  res.json(await deleteItem(req.params.itemID));
+  const response = await restaurantService.deleteItemInRestaurant(
+    req.params.itemID
+  );
+  res.json(response);
 }
 
 async function updateRestaurantByID(req, res) {
-  res.json(await updateRestaurant(req.params.id, req.body));
+  const response = await restaurantService.updateRestaurantByID(
+    req.params.id,
+    req.body
+  );
+  res.json(response);
 }
 
 async function deleteRestaurantByID(req, res) {
-  res.json(await deleteRestaurant(req.params.id));
+  const response = await restaurantService.deleteRestaurantByID(req.params.id);
+  res.json(response);
 }
+
+export default restaurantController;
