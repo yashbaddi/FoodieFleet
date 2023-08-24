e7731ae39fd02e1bd2a891c3369b9c72aadf24cf
Yash Baddi <yashbaddi29@gmail.com>
2023-08-24 10:44:24 +0530

    ✨Added Create New Order in Service

--- Server/services/orders.js
+++ Server/services/orders.js
@@ -2,11 +2,19 @@ import ordersModel from "../model/orders.js";
 
 const ordersService = {
   getOrdersByOrderID: getOrdersByOrderID,
+  createNewOrder: createNewOrder,
 };
 
 async function getOrdersByOrderID(orderID) {
   const readResponse = await ordersModel.readOrder({ id: orderID });
   return readResponse;
+}
+
+async function createNewOrder(userID, restaurantID) {
+  const response = await ordersModel.createOrder(userID, {
+    restaurantID: restaurantID,
+  });
+  return response;
 }
 
 export default ordersService;
