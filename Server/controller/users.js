51f16f1531279909bb0c4aff7ac5ea3f4e2e5f5d
Yash Baddi <yashbaddi29@gmail.com>
2023-08-24 16:10:18 +0530

    ✨ Added Update User Controller

--- Server/controller/users.js
+++ Server/controller/users.js
@@ -3,6 +3,7 @@ import userService from "../services/users";
 const userController = {
   createUser: createUser,
   readUser: readUser,
+  updateUser: updateUser,
 };
 
 async function createUser(req, res) {
@@ -12,5 +13,10 @@ async function createUser(req, res) {
 
 async function readUser(req, res) {
   const response = userService.readUserByID(req.params.id);
+  res.json(response);
+}
+
+async function updateUser(req, res) {
+  const response = userService.updateUser(req.params.id, req.body);
   res.json(response);
 }
