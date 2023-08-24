9aeacc314e2e657819de57ea916969bf9e65230f
Yash Baddi <yashbaddi29@gmail.com>
2023-08-24 11:33:10 +0530

    ✨Added Get Restaurant By ID Service

--- Server/services/restaurants.js
+++ Server/services/restaurants.js
@@ -17,11 +17,18 @@ import itemModel from "../model/items.js";
 
 const restaurantService = {
   getAllRestaurants: getAllRestaurants,
+  getRestaurantsByID: getRestaurantsByID,
 };
+
 async function getAllRestaurants() {
   const data = await restaurantModel.readRestaurant();
   const response = data.map((restaurant) => restaurant.data);
   return response;
+}
+
+async function getRestaurantsByID(restaurnatID) {
+  const data = restaurantModel.readRestaurant({ id: restaurnatID });
+  return data;
 }
 
 export default restaurantService;
