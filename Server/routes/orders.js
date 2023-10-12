97398a6cac6daeaf190c874e670a41ae54367aed
Yash Baddi <yashbaddi29@gmail.com>
2023-10-12 12:59:42 +0530

    ✨Added Auth Middleware to the orders Routes

--- Server/routes/orders.js
+++ Server/routes/orders.js
@@ -2,15 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import orderController from "../controller/orders.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const ordersRouter = express.Router();
 
-ordersRouter.route("/").post(bodyParser.json(), orderController.createNewOrder);
+ordersRouter
+  .route("/")
+  .post(authMiddleware, bodyParser.json(), orderController.createNewOrder);
 
 ordersRouter
   .route("/:id")
-  .get(orderController.getOrdersByOrderID)
-  .patch(bodyParser.json(), orderController.patchCurrentOrder);
+  .get(authMiddleware, orderController.getOrdersByOrderID)
+  .patch(authMiddleware, bodyParser.json(), orderController.patchCurrentOrder);
 
 ordersRouter.route(":id/item/:itemId");
 
