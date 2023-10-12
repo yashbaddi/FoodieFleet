da3f53ea274860cc69c96d296586399de1f7e4c4
Yash Baddi <yashbaddi29@gmail.com>
2023-10-12 13:01:38 +0530

    ✨Added Support for auth Routes

--- Server/app.js
+++ Server/app.js
@@ -1,22 +1,27 @@
 import express from "express";
 import cors from "cors";
 import config from "./config.js";
+import cookieParser from "cookie-parser";
 import restaurantsRouter from "./routes/restaurants.js";
 import ordersRouter from "./routes/orders.js";
 import cartRouter from "./routes/cart.js";
+import authRouter from "./routes/auth.js";
 
 const app = express();
+app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4000"],
+    credentials: true,
   })
 );
 
 app.use("/restaurants", restaurantsRouter);
 app.use("/orders", ordersRouter);
 app.use("/cart", cartRouter);
+app.use("/auth", authRouter);
 
 app.listen(config.app.port, () => {
-  console.log(`applicaton Listening to port 3000 ${config.app.port}`);
+  console.log(`applicaton Listening to port ${config.app.port}`);
 });
