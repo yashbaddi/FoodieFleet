1cbd513e5499cc53aba303299beb58ccab92b158
Yash Baddi <yashbaddi29@gmail.com>
2023-10-12 12:59:15 +0530

    ✨Added Auth Middleware to the cart Routes

--- Server/routes/cart.js
+++ Server/routes/cart.js
@@ -2,12 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import cartCorntroller from "../controller/cart.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const cartRouter = express.Router();
 
 cartRouter
   .route("/")
-  .get(cartCorntroller.readCart)
-  .put(bodyParser.json(), cartCorntroller.updateCart);
+  .get(authMiddleware, cartCorntroller.readCart)
+  .put(authMiddleware, bodyParser.json(), cartCorntroller.updateCart);
 
 export default cartRouter;
