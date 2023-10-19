3cbbe93561d7f45ee2f0a7427ea302c31fd50d9a
Yash Baddi <yashbaddi29@gmail.com>
2023-10-19 16:32:30 +0530

    ⚡️Updated Server Config

--- Server/config.js
+++ Server/config.js
@@ -1,11 +1,18 @@
 const config = {
   app: {
-    port: 3000,
+    port: process.env.SERVER_PORT,
   },
   db: {
-    host: "localhost",
-    port: 27017,
-    name: "db",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+  },
+  oauth: {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
   },
 };
+
 export default config;
