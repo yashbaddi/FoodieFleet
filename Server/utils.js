28ce0e044e5ca9a508b70695ebdf6176f10f2e5c
Yash Baddi <yashbaddi29@gmail.com>
2023-10-19 14:15:58 +0530

    ✨Added generateAuthUrl Util

--- Server/utils.js
+++ Server/utils.js
@@ -14,3 +14,16 @@ export function getUpdateExpression(updatedData) {
 
   //   return { expression: expression.slice(0, -1), values: values };
 }
+
+export function generateAuthUrl() {
+  const params = querystring.stringify({
+    client_id: config.oauth.clientID,
+    redirect_uri: config.oauth.redirectUri,
+    response_type: "code",
+    scope: "profile",
+    state: "randomstring",
+  });
+  console.log("queryString", params);
+  const authURL = "http://localhost:4000/oauth/authorize?" + params.toString();
+  return authURL;
+}
