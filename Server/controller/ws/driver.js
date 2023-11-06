8c053783ba33d49811a6679afeb39db3a6ac4c2c
Yash Baddi <yashbaddi29@gmail.com>
2023-11-06 23:38:06 +0530

    ✨Added update location web socket contoller

--- Server/controller/ws/driver.js
+++ Server/controller/ws/driver.js
@@ -5,10 +5,19 @@ const driverSockets = {};
 
 const driverWsController = {
   addDriverSocket,
+  updateLocation,
 };
 
 async function addDriverSocket(ws) {
   driverSockets[ws.user] = ws;
+}
+
+async function updateLocation(ws, wsRequest) {
+  await driversService.updateDriverLocation(
+    ws.user,
+    wsRequest.data.latitude,
+    wsRequest.data.longitude
+  );
 }
 
 export default driverWsController;
