601c8fc7ed5c27700e27cb12d3b8b4da34b1aa5c
Yash Baddi <yashbaddi29@gmail.com>
2023-11-06 23:09:24 +0530

    ✨Added Redis Setup

--- Server/model/db-connection.js
+++ Server/model/db-connection.js
@@ -1,7 +1,16 @@
 import pg from "pg";
+import { createClient } from "redis";
 import config from "../config.js";
 
 const Pool = pg.Pool;
+
+export const redisClient = createClient();
+
+redisClient.on("error", (err) => {
+  console.log(err);
+});
+
+await redisClient.connect();
 
 const pool = new Pool({
   user: config.db.user,
