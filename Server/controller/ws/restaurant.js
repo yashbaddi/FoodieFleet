799ebe1c9299e06095cd058bad55957409b9225c
Yash Baddi <yashbaddi29@gmail.com>
2023-11-06 23:48:46 +0530

    ✨Added set socket ws contorller

--- Server/controller/ws/restaurant.js
+++ Server/controller/ws/restaurant.js
@@ -3,6 +3,7 @@ const restaurantSockets = [];
 export const restaurantWsController = {
   sendOrderDetails,
   setRestaurantSocket,
+  closeRestaurantSocket,
 };
 
 async function sendOrderDetails(restaurantID, order) {
@@ -15,4 +16,8 @@ async function sendOrderDetails(restaurantID, order) {
 
 async function setRestaurantSocket(ws) {
   restaurantSockets[ws.restaurantID] = ws;
+}
+
+async function closeRestaurantSocket(ws) {
+  restaurantSockets[ws.restaurantID] = undefined;
 }
