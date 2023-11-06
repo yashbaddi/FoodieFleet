19bcb8f896428b9ceaf505f1160426940c1f8f73
Yash Baddi <yashbaddi29@gmail.com>
2023-11-06 23:13:05 +0530

    ✨Added Update Members Location Model

---
+++ Server/model/drivers.js
@@ -0,0 +1,37 @@
+import { redisClient } from "./db-connection.js";
+import pool from "./db-connection.js";
+
+const driversModel = {
+  updateMemberLocation,
+};
+
+async function updateMemberLocation(memberID, location) {
+  console.log("memberID:", memberID, "drivero:", location);
+  const res = await redisClient.geoAdd("driverLocations", [
+    {
+      longitude: location.longitude,
+      latitude: location.latitude,
+      member: memberID,
+    },
+  ]);
+  console.log(res);
+
+  return JSON.parse(res);
+}
+
+export default driversModel;
+// const data = await updateMemberLocation("harsh", {
+//   latitude: 12.9602122,
+//   longitude: 77.6447949,
+// });
+
+// console.log("in data", await data);
+
+// const suo = await readMemberLocation("harsh");
+
+// console.log("location REade:", await suo);
+
+// const duo = await getNearbyMembers({
+//   latitude: 12.9602122,
+//   longitude: 77.6447949,
+// });
