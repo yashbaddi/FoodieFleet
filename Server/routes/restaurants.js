950d0d9dc93cd79f799ef90870d4c823a8f1a9bf
Yash Baddi <yashbaddi29@gmail.com>
2023-11-06 23:53:06 +0530

    ✨Added websocket support for restaurant route

--- Server/routes/restaurants.js
+++ Server/routes/restaurants.js
@@ -2,7 +2,33 @@ import express from "express";
 import bodyParser from "body-parser";
 import restaurantController from "../controller/restaurants.js";
 import { authMiddleware } from "../middlewares/auth.js";
+import { restaurantWsController } from "../controller/ws/restaurant.js";
+import { validateJWTCookie } from "../utils.js";
+import expressWs from "express-ws";
+
 const restaurantsRouter = express.Router();
+
+expressWs(restaurantsRouter);
+
+restaurantsRouter.ws("/ws", bodyParser.json(), (ws, req) => {
+  const payload = {
+    type: "open",
+  };
+
+  try {
+    const user = validateJWTCookie(req.cookies.token);
+    ws.restaurantID = req.body.restaurantID;
+    console.log("restaurant ID in ws connection:", ws.restaurantID);
+  } catch (e) {
+    console.log(e);
+  }
+  restaurantWsController.setRestaurantSocket(ws);
+  ws.send(JSON.stringify(payload));
+
+  ws.on("close", () => {
+    restaurantWsController.closeRestaurantSocket(ws);
+  });
+});
 
 restaurantsRouter
   .route("/")
