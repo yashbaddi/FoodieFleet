1ec9bc905b1bb433ec320253c55f98ec65f1b2a2
Yash Baddi <yashbaddi29@gmail.com>
2023-11-06 23:50:29 +0530

    ✨Added close user socket contoller

--- Server/controller/ws/user.js
+++ Server/controller/ws/user.js
@@ -2,11 +2,16 @@ const userSockets = [];
 
 const userWsController = {
   setUserSocket,
+  closeUserSocket,
 };
 
 async function setUserSocket(ws) {
   console.log(ws.user);
   userSockets[ws.user] = ws;
+}
+
+async function closeUserSocket(ws) {
+  userSockets[ws.user] = undefined;
 }
 
 export default userWsController;
