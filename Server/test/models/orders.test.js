0d75b396cac64b974cf624f84b7ce5f80bf5bf78
Yash Baddi <yashbaddi29@gmail.com>
2023-11-06 23:29:29 +0530

    ✨Added Orders service test cases

--- Server/test/models/orders.test.js
+++ Server/test/models/orders.test.js
@@ -1,4 +1,5 @@
-import { createOrder, readOrder, updateOrder } from "../../model/orders.js";
+import driversService from "../../services/drivers.js";
+import orderService from "../../services/orders.js";
 
 // createOrder(res.locals.userID, {
 //   restaurantID: "b424d354-f611-442e-b384-f80d8597d766",
@@ -15,12 +16,12 @@ import { createOrder, readOrder, updateOrder } from "../../model/orders.js";
 // const data = await readOrder({ id: "fee34d53-2da6-4f96-8549-72cd8efb25bc" });
 // fee34d53-2da6-4f96-8549-72cd8efb25bc sample order id
 
-const data = await updateOrder("fee34d53-2da6-4f96-8549-72cd8efb25bc", {
-  item: {
-    action: "add_item",
-    id: "11ba60f7-f2da-49c0-b10a-43ed2e94268b",
-  },
-});
+// const data = await updateOrder("fee34d53-2da6-4f96-8549-72cd8efb25bc", {
+//   item: {
+//     action: "add_item",
+//     id: "11ba60f7-f2da-49c0-b10a-43ed2e94268b",
+//   },
+// });
 
 // const data = await updateOrder("fee34d53-2da6-4f96-8549-72cd8efb25bc", {
 //   item: {
@@ -29,4 +30,20 @@ const data = await updateOrder("fee34d53-2da6-4f96-8549-72cd8efb25bc", {
 //   },
 // });
 
-console.log(JSON.stringify(data, null, 4));
+const driv = await driversService.updateDriverLocation(
+  "gagan",
+  12.9351088,
+  77.6157508
+);
+
+const data = await orderService.createNewOrder(
+  "yashbaddi",
+  "dd7ee8dd-87c4-42ac-8270-f382b770f4cc",
+  {
+    latitude: 12.9615365,
+    longitude: 77.6441559,
+  }
+);
+console.log("data:", data);
+
+// console.log(JSON.stringify(data, null, 4));
