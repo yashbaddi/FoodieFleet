95acdd54a003ac058c61e673d8f065ed5bc3f234
Yash Baddi <yashbaddi29@gmail.com>
2023-11-06 23:50:54 +0530

    ✨Added send Drivers location Controller

--- Server/controller/ws/user.js
+++ Server/controller/ws/user.js
@@ -3,6 +3,7 @@ const userSockets = [];
 const userWsController = {
   setUserSocket,
   closeUserSocket,
+  sendDriverLocation,
 };
 
 async function setUserSocket(ws) {
@@ -12,6 +13,14 @@ async function setUserSocket(ws) {
 
 async function closeUserSocket(ws) {
   userSockets[ws.user] = undefined;
+}
+
+async function sendDriverLocation(userID, location) {
+  const payload = {
+    type: "driver_location",
+    data: location,
+  };
+  userSockets[userID].send(JSON.stringify(payload));
 }
 
 export default userWsController;
