eb7d0affbbc5a0ddbabe564c5b1f3666bb779d45
Yash Baddi <yashbaddi29@gmail.com>
2023-11-09 17:31:59 +0530

    ✨Added Leaflet and Tailwind

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,31 +4,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-import Home from "./pages/Home/Home";
-import Restaurant from "./pages/Restaurant/Restaurant";
-import Order from "./pages/Order/Order";
+import Home from "./pages/Home";
+import Restaurant from "./pages/Restaurant";
 import Navigation from "./components/navigation";
-import NewRestaurant from "./pages/YourRestaurants/NewRestaurant/NewRestaurant";
-import UpdateRestaurant from "./pages/YourRestaurants/UpdateRestaurant/UpdateRestaurant";
-import CreateItem from "./pages/Restaurant/createItem/createItem";
-import UpdateItem from "./pages/Restaurant/updateItem/updateItem";
+import Cart from "./pages/Cart";
+import TrackOrder from "./pages/TrackOrder";
+import RestaurantDashboard from "./pages/RestaurantDashboard";
+import NewRestaurant from "./pages/NewRestaurant";
+// import UpdateRestaurant from "./components/UpdateRestaurant";
+import UpdateRestaurant from "./pages/UpdateRestaurant";
+// import NewRestaurant from "./components/NewRestaurant";
+// import UpdateRestaurant from "./components/UpdateRestaurant";
+// import CreateItem from "./pages/Restaurant/CreateItem";
+// import UpdateItem from "./components/UpdateItem";
 
 function App() {
+  console.log("Start of the app");
   return (
     <>
       <Provider store={store}>
         {location.pathname !== "/login" && <Navigation />}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/restaurant/new" element={<NewRestaurant />} />
-          <Route path="/restaurant/:id/update" element={<UpdateRestaurant />} />
           <Route path="/restaurant/:id" element={<Restaurant />} />
-          <Route path="restaurant/:id/item/new" element={<CreateItem />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" />
+          <Route path="/track-order/:orderID" element={<TrackOrder />} />
           <Route
-            path="restaurant/:id/item/:itemID/update"
-            element={<UpdateItem />}
+            path="/my-account/restaurants"
+            element={<RestaurantDashboard />}
           />
-          <Route path="/orders" element={<Order />} />
+          <Route
+            path="/my-account/restaurants/new"
+            element={<NewRestaurant />}
+          />
+          <Route
+            path="/my-account/restaurants/:id/update"
+            element={<UpdateRestaurant />}
+          />
         </Routes>
       </Provider>
     </>
@@ -36,3 +49,25 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <Route path="restaurant/:id/item/new" element={<CreateItem />} /> */
+}
+{
+  /* <Route
+            path="restaurant/:id/item/:itemID/update"
+            element={<UpdateItem />}
+          /> */
+}
+{
+  /* <Route path="/users/" */
+}
+{
+  /* <Route
+            path="/restaurant/:id/update"
+            element={<UpdateRestaurant />}
+          />{" "} */
+}
+{
+  /* */
+}
