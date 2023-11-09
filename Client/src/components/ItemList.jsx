0fe0661cb6b67c2f300191f3c2e492e726e93fc9
Yash Baddi <yashbaddi29@gmail.com>
2023-11-09 19:00:59 +0530

    ✨Added Item List

---
+++ Client/src/components/ItemList.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import ItemContainer from "../components/ItemContainer";
+import { getRestaurantMenu } from "../services/requests";
+
+export default function ItemsList({ id }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getRestaurantMenu(id).then((data) => setItems(data));
+  }, [id]);
+
+  function updateList(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  console.log("Items", items);
+  return (
+    <>
+      List of Items
+      {items.map((item) => {
+        return <ItemContainer key={item.id} itemData={item} />;
+      })}
+    </>
+  );
+}
