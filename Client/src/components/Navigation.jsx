12168d6ac53944de8e8af26c30cdf720dff22ea6
Yash Baddi <yashbaddi29@gmail.com>
2023-11-09 19:04:12 +0530

    Fixed Navigation Bar

--- Client/src/components/Navigation.jsx
+++ Client/src/components/Navigation.jsx
@@ -1,22 +1,52 @@
 import { Link } from "react-router-dom";
-import { orderID } from "../Data/sampleData.js";
-
+import Logo from "./Logo.jsx";
+import { FaRegUser } from "react-icons/fa6";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { IoLocation } from "react-icons/io5";
+import { RiShoppingCart2Fill } from "react-icons/ri";
+import ProfileDropMenu from "./ProfileDropMenu.jsx";
+import { useState } from "react";
 export default function Navigation() {
+  const [isProfileDropDownOpen, setProfileDropdownOpen] = useState(false);
+  function toggleProfileDropdown() {
+    setProfileDropdownOpen(!isProfileDropDownOpen);
+  }
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light navigation">
-        <Link to="/">
-          <a className="navbar-brand navigation__header">Foodie Fleet</a>
-        </Link>
-
-        <ul className="navbar-nav">
-          <li className="nav-item dropdown navigation__item--address">
-            Address
-          </li>
-          <Link to={"/orders/"}>
-            <li className="nav-item navigation__item--cart">Cart</li>
+      <nav className="bg-white flex items-center justify-between p-6">
+        <div className="flex basis-1/2">
+          <Link to="/">
+            <a className="border-red-200 h-1">
+              <Logo />
+            </a>
           </Link>
-        </ul>
+          <IoLocation />
+          <a className=" text-orange-400 no-underline">Address</a>
+        </div>
+
+        <div className="flex justify-center items-center p-10 basis-1/4 gap-4">
+          <div className="flex items-baseline">
+            <BiSearchAlt2 />
+            <p className="inline">Search</p>
+          </div>
+
+          <div className="flex items-baseline no-underline">
+            <RiShoppingCart2Fill />
+            <Link
+              to={"/cart/"}
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <p className="no-underline text-black">Cart</p>
+            </Link>
+          </div>
+          <div className="flex items-baseline">
+            <FaRegUser />
+            <button onClick={toggleProfileDropdown} className="inline">
+              Profile
+            </button>
+          </div>
+        </div>
+        {isProfileDropDownOpen && <ProfileDropMenu />}
       </nav>
     </>
   );
