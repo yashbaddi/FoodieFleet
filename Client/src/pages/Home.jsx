73470396cf9268c177c55de7197fc0b954c80c4b
Yash Baddi <yashbaddi29@gmail.com>
2023-11-09 17:33:10 +0530

    ✨Added A Home Page

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import HomePageContainer from "../components/HomePageContainer";
+
+export default function Home() {
+  console.log("in the main page");
+  return (
+    <>
+      Home Page
+      <HomePageContainer />
+    </>
+  );
+}
