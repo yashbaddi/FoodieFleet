e84e4284c7422c724035c3bd21980557ca836ed1
Yash Baddi <yashbaddi29@gmail.com>
2023-11-10 11:03:37 +0530

    ✨Added Update Driver Status

--- Server/controller/driver.js
+++ Server/controller/driver.js
@@ -2,9 +2,18 @@ import driversService from "../services/drivers";
 
 export const driverController = {
   getDriverDetails,
+  updateDriverStatus,
 };
 
 async function getDriverDetails(req, res) {
   const response = await driversService.getDriverStatus(res.locals.userID);
+  res.json(response);
+}
+
+async function updateDriverStatus(req, res) {
+  const response = await driversService.updateDriverStatus(
+    res.locals.userID,
+    req.query.status
+  );
   res.json(response);
 }
