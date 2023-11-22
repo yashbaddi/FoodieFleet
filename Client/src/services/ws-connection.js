696756e61f7e1a8bc2f79afcc43456266c65b825
Yash Baddi <yashbaddi29@gmail.com>
2023-11-22 17:34:37 +0530

    🔨Fixed Web socket connections with callbacks

--- Client/src/services/ws-connection.js
+++ Client/src/services/ws-connection.js
@@ -5,7 +5,7 @@ import { updateDriverLocation } from "../store/actionCreators/driverLocation.act
 
 const serverUrl = "ws://localhost:3000/";
 
-export function createUserSocketConnect() {
+export function createUserSocketConnect(getNotifications) {
   const wsClient = new WebSocket(serverUrl + "user/ws");
   wsClient.onopen = (ws) => {
     console.log("user socket connection success");
@@ -17,20 +17,22 @@ export function createUserSocketConnect() {
     const msg = JSON.parse(message.data.toString());
     if (msg.type === "open") {
     }
-    if (msg.type === "driver_location") {
-      console.log("msg:", msg);
-
+    if (msg.type === "partner_location") {
       store.dispatch(updateDriverLocation(msg.data));
     }
     if (msg.type === "notification") {
+      getNotifications(msg.data);
     }
+    // if (msg.type === "partner_information") {
+    //   setPartnerInformation(msg.data);
+    // }
   };
   wsClient.onclose = () => {
     clearInterval(interval);
   };
   return wsClient;
 }
-export function createRestaurantSocketConnect() {
+export function createRestaurantSocketConnect(setNewOrder, getNotifications) {
   const wsClient = new WebSocket(serverUrl + "restaurants/ws");
   wsClient.onopen = (ws) => {
     console.log("restaurant socket connection success");
@@ -42,6 +44,10 @@ export function createRestaurantSocketConnect() {
     const msg = JSON.parse(message.data.toString());
     console.log("msg:", msg);
     if (msg.type === "order") {
+      setNewOrder(msg.data);
+    }
+    if (msg.type === "notification") {
+      getNotifications(msg.data);
     }
   };
   wsClient.onclose = () => {
@@ -49,7 +55,7 @@ export function createRestaurantSocketConnect() {
   };
   return wsClient;
 }
-export function createDriverSocketConnect() {
+export function createDriverSocketConnect(setNewOrder) {
   const wsClient = new WebSocket(serverUrl + "driver/ws");
   wsClient.onopen = (ws) => {
     console.log("driver socket connection success");
@@ -61,6 +67,7 @@ export function createDriverSocketConnect() {
     const msg = JSON.parse(message.data.toString());
     console.log("msg:", msg);
     if (msg.type === "order") {
+      setNewOrder();
     }
   };
   wsClient.onclose = () => {
@@ -85,13 +92,69 @@ export function updateDriverStatusWS(ws, status) {
   ws.send(JSON.stringify(payload));
 }
 
-export function updateOrderStatusWS(ws, orderID, status) {
+export function approveOrderWS(ws, orderID) {
   const payload = {
-    type: "orderStatus",
+    type: "status",
     data: {
-      orderID,
-      status,
+      orderID: orderID,
+      status: "PREPARING",
     },
   };
+  ws.send(JSON.stringify(payload));
+}
+
+export function rejectOrderWS(ws, orderID) {
+  const payload = {
+    type: "status",
+    data: {
+      orderID: orderID,
+      status: "REJECTED",
+    },
+  };
+  ws.send(JSON.stringify(payload));
+}
+export function setOrderToDeliveringWS(ws, orderID) {
+  const payload = {
+    type: "status",
+    data: {
+      orderID: orderID,
+      status: "DELIVERING",
+    },
+  };
+  ws.send(JSON.stringify(payload));
+}
+
+export function setOrderToDeliveredWS(ws, orderID) {
+  const payload = {
+    type: "status",
+    data: {
+      orderID: orderID,
+      status: "DELIVERED",
+    },
+  };
+  ws.send(JSON.stringify(payload));
+}
+
+// export function updateOrderStatusWS(ws, orderID, status) {
+//   console.log("Update order Status has been sent", { orderID, status });
+//   const payload = {
+//     type: "orderStatus",
+//     data: {
+//       orderID,
+//       status,
+//     },
+//   };
+//   console.log("socket", ws);
+//   ws.send(JSON.stringify(payload));
+// }
+
+export function getDriverLocationWS(ws, driverID) {
+  const payload = {
+    type: "get_driver_location",
+    data: {
+      driverID,
+    },
+  };
+  console.log("socket", ws);
   ws.send(JSON.stringify(payload));
 }
