c3c94e37fc5ed8fdeb1b6a6ac4cbf81831764e87
Yash Baddi <yashbaddi29@gmail.com>
2023-11-24 00:14:46 +0530

    🧹Removed Redundat Imports

--- Client/src/pages/DriversDashboard.jsx
+++ Client/src/pages/DriversDashboard.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import {
   createDriverSocketConnect,
   sendLocationWS,
-  updateDriverStatusWS,
 } from "../services/ws-connection";
 import { getDriversDetails } from "../services/requests";
 import useGeolocation from "../hooks/useGeoLocation";
