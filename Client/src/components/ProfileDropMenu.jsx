203dac4018d9e692f26e20cf6e5d3316780722e5
Yash Baddi <yashbaddi29@gmail.com>
2023-11-24 00:14:20 +0530

    ✨Added Links to profile drop down

--- Client/src/components/ProfileDropMenu.jsx
+++ Client/src/components/ProfileDropMenu.jsx
@@ -1,13 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ProfileDropMenu() {
   return (
     <div className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-52">
       <ul>
-        <li className="hover:bg-orange-500">Orders</li>
-        <li className="hover:bg-orange-500">Driver Dashboard</li>
-        <li className="hover:bg-orange-500">Restaurant Dashboard</li>
-        <li className="hover:bg-orange-500">Sign Out</li>
+        <li className="hover:bg-orange-500">
+          <Link
+            to={"/orders/"}
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <p className="no-underline text-black">Orders</p>
+          </Link>
+        </li>
+        <li className="hover:bg-orange-500">
+          <li className="hover:bg-orange-500">
+            <Link
+              to={"/my-account/delivery-partner"}
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <p className="no-underline text-black">Driver's Dashboard</p>
+            </Link>
+          </li>
+        </li>
+        <li className="hover:bg-orange-500">
+          <li className="hover:bg-orange-500">
+            <Link
+              to={"/my-account/restaurant-admin"}
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <p className="no-underline text-black">Restaurant Dashboard</p>
+            </Link>
+          </li>
+        </li>
+        <li className="hover:bg-orange-500">
+          <li className="hover:bg-orange-500">
+            <Link
+              to={"/orders/"}
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <p className="no-underline text-black">Sign Out</p>
+            </Link>
+          </li>
+        </li>
       </ul>
     </div>
   );
