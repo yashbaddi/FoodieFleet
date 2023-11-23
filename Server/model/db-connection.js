ffdbcc0168b62fc5ca00a69d0bcc48125a0eaaca
Yash Baddi <yashbaddi29@gmail.com>
2023-11-24 00:02:06 +0530

    ⚡️ Updated url support for redis connection

--- Server/model/db-connection.js
+++ Server/model/db-connection.js
@@ -1,22 +1,23 @@
 import pg from "pg";
-import { createClient } from "redis";
+import { Redis } from "ioredis";
 import config from "../config.js";
 
 const Pool = pg.Pool;
 
-export const redisClient = createClient();
+export const redisClient = new Redis(config.redis.url);
 
 redisClient.on("error", (err) => {
   console.log(err);
 });
 
-await redisClient.connect();
+// await redisClient.connect();
 
 const pool = new Pool({
   user: config.db.user,
   host: config.db.host,
   database: config.db.name,
   port: config.db.port,
+  password: config.db.password,
 });
 
 export default pool;
