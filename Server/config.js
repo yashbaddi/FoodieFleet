43ea6f860c8cb56a55cb3a887ba0857e89fb29e0
Yash Baddi <yashbaddi29@gmail.com>
2023-11-24 00:00:16 +0530

    ✨Added Redis Infromation to config

--- Server/config.js
+++ Server/config.js
@@ -4,17 +4,23 @@ dotenv.config({ path: "./.env" });
 const config = {
   app: {
     port: Number(process.env.SERVER_PORT),
+    corsOrigin: process.env.CORS_URLS ? process.env.CORS_URLS.split(" ") : [],
   },
   db: {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10),
     name: process.env.DB_NAME,
     user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
   oauth: {
+    providerURL: process.env.OAUTH_PROVIDER,
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     redirectUri: process.env.REDIRECT_URI,
+  },
+  redis: {
+    url: process.env.REDIS_URL,
   },
 };
 
