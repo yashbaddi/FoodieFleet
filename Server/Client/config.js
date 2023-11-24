fafb3efd63cb7efbddc14278b93e8bc200546833
Yash Baddi <yashbaddi29@gmail.com>
2023-11-24 13:29:53 +0530

    🔨Fixed web socket to use secure

--- Server/Client/config.js
+++ Server/Client/config.js
@@ -1,7 +1,7 @@
 const config = {
   api: {
     url: "https://foodie-fleet-node-server.onrender.com/api/",
-    wsURL: "ws://foodie-fleet-node-server.onrender.com/api/",
+    wsURL: "wss://foodie-fleet-node-server.onrender.com/api/",
   },
 };
 
