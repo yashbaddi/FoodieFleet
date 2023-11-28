c91bc5981545b26aafbcc0a84c626ff0ca83a474
Yash Baddi <yashbaddi29@gmail.com>
2023-11-28 21:33:57 +0530

    ✨Added test for get Nearby members

--- Server/test/models/driver.test.js
+++ Server/test/models/driver.test.js
@@ -1,3 +1,9 @@
+import driversModel from "../../model/drivers.js";
 import driversService from "../../services/drivers.js";
 
-driversService.updateDriverLocation("gagan", 12.9335207, 77.6204045);
+// driversService.updateDriverLocation("gagan", 12.9335207, 77.6204045);
+
+driversModel.getNearbyMembers(
+  { latitude: 12.9607019, longitude: 77.6432458 },
+  200
+);
