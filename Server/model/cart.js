5e10f93f3068f12ae41694f95292e2371c8f88cd
Yash Baddi <yashbaddi29@gmail.com>
2023-11-28 21:23:51 +0530

    🔨Fixed Multiple Restaurant in same cart issue

--- Server/model/cart.js
+++ Server/model/cart.js
@@ -1,13 +1,17 @@
 import pool from "./db-connection.js";
 
 const cartModel = {
-  updateQuantityInCart: updateQuantityInCart,
-  removeItemFromCart: removeItemFromCart,
-  readItemsInCart: readItemsInCart,
+  updateQuantityInCart,
+  removeItemFromCart,
+  readItemsInCart,
+  clearItemsInCart,
 };
 
 async function updateQuantityInCart(userID, itemID, quantity = 1) {
   console.log("userID", userID, "itemID", itemID, "quantity", quantity);
+
+  await removeDifferentRestaurantCartItems(userID, itemID);
+
   const query = `INSERT INTO cart_items (user_id,item_id,quantity) VALUES($1,$2,$3) 
         ON CONFLICT(user_id,item_id) 
         DO UPDATE SET quantity=$3 RETURNING quantity`;
@@ -19,6 +23,20 @@ async function updateQuantityInCart(userID, itemID, quantity = 1) {
     itemID: itemID,
     quantity: Number(quantity),
   };
+}
+
+async function removeDifferentRestaurantCartItems(userID, itemID) {
+  const restaurantID = (
+    await pool.query(`SELECT restaurant_id from items where id=$1`, [itemID])
+  ).rows[0]?.restaurant_id;
+  console.log(restaurantID);
+
+  if (restaurantID) {
+    await pool.query(
+      `DELETE FROM cart_items using items WHERE item_id=items.id AND user_id=$1 AND items.restaurant_id!=$2`,
+      [userID, restaurantID]
+    );
+  }
 }
 
 async function removeItemFromCart(userID, itemID) {
@@ -51,6 +69,14 @@ async function readItemsInCart(userID) {
   //   console.log(restaurantRes);
   // }
   return itemResponse;
+}
+
+async function clearItemsInCart(userID) {
+  const dataResponse = await pool.query(
+    `DELETE FROM cart_items WHERE user_id=$1`,
+    [userID]
+  );
+  return dataResponse;
 }
 
 export default cartModel;
