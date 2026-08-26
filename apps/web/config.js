c121a872dd7c664e2f65883a2c4daa36f28515d1
Yash Baddi <yashbaddi29@gmail.com>
2026-08-26 14:17:40 +0530

    update client uri

--- apps/web/config.js
+++ apps/web/config.js
@@ -1,7 +1,7 @@
 const config = {
   api: {
-    url: import.meta.env.APP_API_URL || "http://localhost:8001/api/",
-    wsURL: import.meta.env.APP_WS_URL || "ws://localhost:8001/api/",
+    url: "/api/",
+    wsURL: `${window.location.protocol === "https:" ? "wss:" : "ws:"}//${window.location.host}/api/`,
   },
 };
 
